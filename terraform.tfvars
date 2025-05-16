3f29a30bfb6f848012e60bb719b971ec4a2a18ae
Blossom <bndikum22@gmail.com>
2025-05-15 22:51:50 -0700

    terraform code

---
+++ terraform.tfvars
@@ -0,0 +1,4 @@
+ami = "ami-0e58b56aa4d64231b"
+region_name = "us-east-1"
+profile_name = "default"
+instanceType = "t2.micro"
---
+++ variables.tf
@@ -0,0 +1,14 @@
+variable "ami" {
+  description = "Amazon machine image"
+  type = string
+  default = "ami-0e58b56aa4d64231b"
+}
+variable "instanceType" {
+  default = "t2.small"
+}
+variable "region_name" {
+  #default = "us-east-1"
+}
+variable "profile_name" {
+  default = "default"
+}
